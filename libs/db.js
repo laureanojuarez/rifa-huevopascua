a3250fbff43a084f34b654653a49f4cbeddf24b2
Laureano <laureanojuarez69@gmail.com>
2026-03-15 22:54:47 -0300

    feat: Add fallback database URL for Sequelize initialization.

--- libs/db.js
+++ libs/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+export const sequelize = new Sequelize(process.env.DATABASE_URL || "postgres://dummy:dummy@localhost/dummy", {
     dialect: 'postgres',
     ssl: true,
     dialectOptions: {
